fa506b267e6e927232210d6d0f748568d152d5bc
j1nx5 <j1nx@posteo.de>
2025-12-30 22:06:52 +0100

    some correction

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,18 @@ const finService = new FinService();
 const symbol: string[] = finService.loadYamlFile('./symbols.yaml');
 
 (async () => {
+    
     for(const symb of symbol){
         if(await finService.check_symbol_in_db(symb)){
-            let last_date = Number(await finService.get_last_datetime_o_s(symb)|| 0)
-            let ldUTCString = new Date(last_date+1).toUTCString();
-            await finService.call_chart(symb, ldUTCString, nowUTCString, '1m') 
+            let last_date = await finService.get_last_datetime_o_s(symb)
+            let ldUTCString = new Date(Number(last_date) * 1000 + 1000).toUTCString()
+            console.time('call_chart to fill up')
+            await finService.call_chart(symb, ldUTCString, nowUTCString, '1m')
+            console.timeEnd('call_chart to fill up')
         } else {
+            console.time('call_chart for new data')
             await finService.call_chart(symb, days8Befor, nowUTCString, '1m')
+            console.timeEnd('call_chart for new data')
         }
     }
 })();
