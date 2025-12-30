59ceef5df5f2742da8adfbdc659367bfea87df89
j1nx5 <j1nx@posteo.de>
2025-12-30 18:32:04 +0100

    build intervall for running

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
-import cron from 'node-cron';
 import { FinService } from "./finservice.js";
 
 const finService = new FinService();
-// const check = await finService.check_symbol_in_db('AAPL')
-//console.log(check)
 
 console.time("Initial Process Duration");
 await finService.initialProcess()
 console.timeEnd("Initial Process Duration");
 
-console.time("Filling Chart Data Duration");
-await finService.fill_chart_data();
-console.timeEnd("Filling Chart Data Duration");
+setInterval( async () => {
+    console.time("Filling Chart Data Duration");
+    await finService.fill_chart_data();
+    console.timeEnd("Filling Chart Data Duration");
+}, 1200000);
 
 
-//const quote = await finService.call_quote('IBM');
+
 
 // const now = new Date()
 // const nowUTCString= now.toUTCString()
@@ -22,10 +21,6 @@ console.timeEnd("Filling Chart Data Duration");
 
 //const res = await finService.call_chart('IBM', days8Befor, nowUTCString, '1m');
 // console.log(res)
-
-// finService.call_bs_quarterly()
-
-//const quote = await finService.call_quote('IBM');
 
 // cron.schedule('* * * * *', async () => {
 //     try {
