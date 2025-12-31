43e0c8dc8cc45f031c751012fb426945f072461e
j1nx5 <j1nx@posteo.de>
2025-12-31 13:04:34 +0100

    add logs

--- src/index.ts
+++ src/index.ts
@@ -10,24 +10,39 @@ const sleep = (ms: number) =>
     while(true){
         for(const symb of symbol){
             if(await finService.check_symbol_in_db(symb)){
+                console.log(`Run case symbol exist`)
                 let last_date = await finService.get_last_datetime_o_s(symb)
                 let ldUTCString = new Date(Number(last_date) * 1000 + 1000).toUTCString()
                 let now: Date = new Date()
                 let nowUTCString: string= now.toUTCString()
-                let days8Befor: string = new Date(now.getTime() - 8 * 24 * 60 * 60 * 1000).toUTCString()
-                console.time('call_chart to fill up')
+                // console.time('call_chart to fill up')
+                console.log(`Run call_chart with signature 
+                    last_date:${last_date}, 
+                    ldUTCString:${ldUTCString},
+                    now:${now},
+                    last_date:${last_date},
+                    nowUTCString:${nowUTCString}
+                `)
                 await finService.call_chart(symb, ldUTCString, nowUTCString, '1m')
-                console.timeEnd('call_chart to fill up')
+                // console.timeEnd('call_chart to fill up')
             } else {
+                console.log(`Run case symbol not exist`)
                 let now: Date = new Date()
                 let nowUTCString: string= now.toUTCString()
                 let days8Befor: string = new Date(now.getTime() - 8 * 24 * 60 * 60 * 1000).toUTCString()
-                console.time('call_chart for new data')
+                // console.time('call_chart for new data')
+                console.log(`Run call_chart with signature 
+                    now:${now}, 
+                    nowUTCString:${nowUTCString},
+                    days8Befor: ${days8Befor}
+                `)
                 await finService.call_chart(symb, days8Befor, nowUTCString, '1m')
-                console.timeEnd('call_chart for new data')
+                // console.timeEnd('call_chart for new data')
             }
+            console.log('go sleep 50 sec.')
             await sleep(50000)
         }
+        console.log('wait for next round 20 min.')
         await sleep(200000)
     }
 })();
